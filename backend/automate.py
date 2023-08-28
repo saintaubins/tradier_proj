6a7400cccac85edc6ad76848cb776561dc51b1f7
Sem Saint-Aubin <semsaint-aubin@Sems-MacBook-Air.local>
2023-08-28 12:55:13 -0400

    fixed bug

--- backend/automate.py
+++ backend/automate.py
@@ -269,10 +269,10 @@ def figure_it_out(d: dict, loop_the_trend: bool, first_call: str):
                 ema1, ema7, data_array = monitor_the_trade(d)
 
                 # global message
-                res = update_status(suggested_direction, direction,
-                                    exit_the_trade, loop_the_trend, ema1, ema7, data_array, option_symbol)
+                # res = update_status(suggested_direction, direction,
+                #                     exit_the_trade, loop_the_trend, ema1, ema7, data_array, option_symbol)
                 logging.info(f"ema1:{ema1[-1]}, ema7:{ema7[-1]}")
-                logging.info(f'#########res {res}')
+                # logging.info(f'#########res {res}')
                 if ema1[-1] > ema7[-1]:
                     suggested_direction = 'long'
                 if ema1[-1] < ema7[-1]:
@@ -308,12 +308,12 @@ def figure_it_out(d: dict, loop_the_trend: bool, first_call: str):
                     print('ema7 ->', ema7[-1])
                     # print('data_array ->', data_array[-1])
                     logging.info(
-                        f"data_array:{data_array[-1]}")
+                        f"data_array: {data_array[-1]}")
                     # print('option_symbol ->', option_symbol)
 
                 # global message
-                # update_status(suggested_direction, direction,
-                #                   exit_the_trade, loop_the_trend, ema1, ema7, data_array, option_symbol)
+                update_status(suggested_direction, direction,
+                              exit_the_trade, loop_the_trend, ema1, ema7, data_array, option_symbol)
 
                 time.sleep(5)
 

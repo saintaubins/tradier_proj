e039826b9d139ba644cee5fe66ed4272cbd07304
Sem Saint-Aubin <semsaint-aubin@Sems-MacBook-Air.local>
2023-08-30 10:51:22 -0400

    res

--- backend/app.py
+++ backend/app.py
@@ -429,7 +429,7 @@ def figure_it_out():
             d=current_trade, loop_the_trend=loop_the_trend, first_call=first_call)
 
         # print('res from figure it out res->', res)
-        if first_call == 'True':
+        if first_call == True:
             print('res from figure it out res->', res)
             response_data = {'message': res}
         else:

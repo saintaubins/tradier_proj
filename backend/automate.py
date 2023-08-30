35e39f4a223d0a4a625e05f8dd401768a1afdd24
Sem Saint-Aubin <semsaint-aubin@Sems-MacBook-Air.local>
2023-08-30 15:30:32 -0400

    first call

--- backend/automate.py
+++ backend/automate.py
@@ -286,6 +286,7 @@ def update_status(suggested_direction, direction, exit_the_trade, loop_the_trend
 def figure_it_out(d: dict, loop_the_trend: bool, first_call: str):
     message = {}
     first_call = first_call.replace("'", " ")
+    logging.info(f'first_call 289: {first_call}')
     try:
         exit_the_trade = False
         suggested_direction = 'dunno yet'

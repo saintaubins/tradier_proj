1cc6572513563a847614b4f5f44d17b3f8c16289
Sem Saint-Aubin <semsaint-aubin@Sems-MacBook-Air.local>
2023-09-05 13:30:31 -0400

    resolve promise

--- frontend/automate.js
+++ frontend/automate.js
@@ -121,7 +121,7 @@ document.getElementById("modalYesButton").addEventListener("click", function() {
                           showAfterOrderMessage([`trade is fulfilled, still waiting for confirmation`]);
                       } 
                       else {
-                          showAfterOrderMessage([`${afterTrade2.message}`, `${afterTrade2.message}`]);
+                          showAfterOrderMessage([`${promise2.status}`, `${afterTrade2.message}`]);
                       }
                   })
                   .catch((error) => {

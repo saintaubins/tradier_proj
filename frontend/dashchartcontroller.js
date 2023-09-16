9f6383c83347109fb46a98ce8ab0b8645bf70c0a
Sem Saint-Aubin <semsaint-aubin@Sems-MacBook-Air.local>
2023-09-16 17:10:34 -0400

    colors

--- frontend/dashchartcontroller.js
+++ frontend/dashchartcontroller.js
@@ -334,7 +334,7 @@ function updateChartWithData(newDataArray) {
       data: [],
       // fill: false,
       backgroundColor: 'red',
-      borderColor: 'lightred',
+      borderColor: 'rgb(237,168,57)',
       // pointRadius: 0,
       // tension: 0.25
     },{
@@ -342,7 +342,7 @@ function updateChartWithData(newDataArray) {
       data: [],
       // fill: false,
       backgroundColor: 'green',
-      borderColor: 'lightgreen',
+      borderColor: 'rgb(254,211,211)',
       // pointRadius: 0,
       // tension: 0.25
     }]
